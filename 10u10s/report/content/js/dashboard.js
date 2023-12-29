f29cfcb7104314c957e1a06bee94ee2c9f5cdb7a
root <root@ip-10-10-44-12.ap-south-1.compute.internal>
2023-12-29 07:24:32 +0000

    10u10s

---
+++ 10u10s/report/content/js/dashboard.js
@@ -0,0 +1,235 @@
+/*
+   Licensed to the Apache Software Foundation (ASF) under one or more
+   contributor license agreements.  See the NOTICE file distributed with
+   this work for additional information regarding copyright ownership.
+   The ASF licenses this file to You under the Apache License, Version 2.0
+   (the "License"); you may not use this file except in compliance with
+   the License.  You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+var showControllersOnly = false;
+var seriesFilter = "";
+var filtersOnlySampleSeries = true;
+
+/*
+ * Add header in statistics table to group metrics by category
+ * format
+ *
+ */
+function summaryTableHeader(header) {
+    var newRow = header.insertRow(-1);
+    newRow.className = "tablesorter-no-sort";
+    var cell = document.createElement('th');
+    cell.setAttribute("data-sorter", false);
+    cell.colSpan = 1;
+    cell.innerHTML = "Requests";
+    newRow.appendChild(cell);
+
+    cell = document.createElement('th');
+    cell.setAttribute("data-sorter", false);
+    cell.colSpan = 3;
+    cell.innerHTML = "Executions";
+    newRow.appendChild(cell);
+
+    cell = document.createElement('th');
+    cell.setAttribute("data-sorter", false);
+    cell.colSpan = 7;
+    cell.innerHTML = "Response Times (ms)";
+    newRow.appendChild(cell);
+
+    cell = document.createElement('th');
+    cell.setAttribute("data-sorter", false);
+    cell.colSpan = 1;
+    cell.innerHTML = "Throughput";
+    newRow.appendChild(cell);
+
+    cell = document.createElement('th');
+    cell.setAttribute("data-sorter", false);
+    cell.colSpan = 2;
+    cell.innerHTML = "Network (KB/sec)";
+    newRow.appendChild(cell);
+}
+
+/*
+ * Populates the table identified by id parameter with the specified data and
+ * format
+ *
+ */
+function createTable(table, info, formatter, defaultSorts, seriesIndex, headerCreator) {
+    var tableRef = table[0];
+
+    // Create header and populate it with data.titles array
+    var header = tableRef.createTHead();
+
+    // Call callback is available
+    if(headerCreator) {
+        headerCreator(header);
+    }
+
+    var newRow = header.insertRow(-1);
+    for (var index = 0; index < info.titles.length; index++) {
+        var cell = document.createElement('th');
+        cell.innerHTML = info.titles[index];
+        newRow.appendChild(cell);
+    }
+
+    var tBody;
+
+    // Create overall body if defined
+    if(info.overall){
+        tBody = document.createElement('tbody');
+        tBody.className = "tablesorter-no-sort";
+        tableRef.appendChild(tBody);
+        var newRow = tBody.insertRow(-1);
+        var data = info.overall.data;
+        for(var index=0;index < data.length; index++){
+            var cell = newRow.insertCell(-1);
+            cell.innerHTML = formatter ? formatter(index, data[index]): data[index];
+        }
+    }
+
+    // Create regular body
+    tBody = document.createElement('tbody');
+    tableRef.appendChild(tBody);
+
+    var regexp;
+    if(seriesFilter) {
+        regexp = new RegExp(seriesFilter, 'i');
+    }
+    // Populate body with data.items array
+    for(var index=0; index < info.items.length; index++){
+        var item = info.items[index];
+        if((!regexp || filtersOnlySampleSeries && !info.supportsControllersDiscrimination || regexp.test(item.data[seriesIndex]))
+                &&
+                (!showControllersOnly || !info.supportsControllersDiscrimination || item.isController)){
+            if(item.data.length > 0) {
+                var newRow = tBody.insertRow(-1);
+                for(var col=0; col < item.data.length; col++){
+                    var cell = newRow.insertCell(-1);
+                    cell.innerHTML = formatter ? formatter(col, item.data[col]) : item.data[col];
+                }
+            }
+        }
+    }
+
+    // Add support of columns sort
+    table.tablesorter({sortList : defaultSorts});
+}
+
+$(document).ready(function() {
+
+    // Customize table sorter default options
+    $.extend( $.tablesorter.defaults, {
+        theme: 'blue',
+        cssInfoBlock: "tablesorter-no-sort",
+        widthFixed: true,
+        widgets: ['zebra']
+    });
+
+    var data = {"OkPercent": 100.0, "KoPercent": 0.0};
+    var dataset = [
+        {
+            "label" : "FAIL",
+            "data" : data.KoPercent,
+            "color" : "#FF6347"
+        },
+        {
+            "label" : "PASS",
+            "data" : data.OkPercent,
+            "color" : "#9ACD32"
+        }];
+    $.plot($("#flot-requests-summary"), dataset, {
+        series : {
+            pie : {
+                show : true,
+                radius : 1,
+                label : {
+                    show : true,
+                    radius : 3 / 4,
+                    formatter : function(label, series) {
+                        return '<div style="font-size:8pt;text-align:center;padding:2px;color:white;">'
+                            + label
+                            + '<br/>'
+                            + Math.round10(series.percent, -2)
+                            + '%</div>';
+                    },
+                    background : {
+                        opacity : 0.5,
+                        color : '#000'
+                    }
+                }
+            }
+        },
+        legend : {
+            show : true
+        }
+    });
+
+    // Creates APDEX table
+    createTable($("#apdexTable"), {"supportsControllersDiscrimination": true, "overall": {"data": [0.61, 500, 1500, "Total"], "isController": false}, "titles": ["Apdex", "T (Toleration threshold)", "F (Frustration threshold)", "Label"], "items": [{"data": [0.0, 500, 1500, "Feed_Page"], "isController": false}, {"data": [1.0, 500, 1500, "otp verify Login"], "isController": false}, {"data": [0.05, 500, 1500, "Login"], "isController": false}, {"data": [1.0, 500, 1500, "Get Requested OTP"], "isController": false}, {"data": [1.0, 500, 1500, "Create installation"], "isController": false}]}, function(index, item){
+        switch(index){
+            case 0:
+                item = item.toFixed(3);
+                break;
+            case 1:
+            case 2:
+                item = formatDuration(item);
+                break;
+        }
+        return item;
+    }, [[0, 0]], 3);
+
+    // Create statistics table
+    createTable($("#statisticsTable"), {"supportsControllersDiscrimination": true, "overall": {"data": ["Total", 50, 0, 0.0, 1577.5999999999997, 7, 7771, 184.5, 4831.5, 6787.449999999998, 7771.0, 3.012592637223595, 117.47757972073265, 1.7257683994095316], "isController": false}, "titles": ["Label", "#Samples", "FAIL", "Error %", "Average", "Min", "Max", "Median", "90th pct", "95th pct", "99th pct", "Transactions/s", "Received", "Sent"], "items": [{"data": ["Feed_Page", 10, 0, 0.0, 4968.900000000001, 3181, 7771, 4271.5, 7695.8, 7771.0, 7771.0, 0.7811279487580066, 150.68598900562412, 0.4653203600999844], "isController": false}, {"data": ["otp verify Login", 10, 0, 0.0, 236.3, 104, 447, 184.5, 441.1, 447.0, 447.0, 1.0650761529449357, 1.1472451139631485, 0.644870326978379], "isController": false}, {"data": ["Login", 10, 0, 0.0, 2612.3, 1368, 4845, 2247.0, 4734.900000000001, 4845.0, 4845.0, 0.9338812103100486, 0.25262216333582366, 0.5499320017743743], "isController": false}, {"data": ["Get Requested OTP", 10, 0, 0.0, 36.1, 7, 130, 21.5, 124.50000000000003, 130.0, 130.0, 1.0698619878035733, 0.24970411629399808, 0.5850807745800792], "isController": false}, {"data": ["Create installation", 10, 0, 0.0, 34.4, 7, 107, 14.5, 103.80000000000001, 107.0, 107.0, 1.1340440009072352, 0.5537324223179859, 0.5980310161034248], "isController": false}]}, function(index, item){
+        switch(index){
+            // Errors pct
+            case 3:
+                item = item.toFixed(2) + '%';
+                break;
+            // Mean
+            case 4:
+            // Mean
+            case 7:
+            // Median
+            case 8:
+            // Percentile 1
+            case 9:
+            // Percentile 2
+            case 10:
+            // Percentile 3
+            case 11:
+            // Throughput
+            case 12:
+            // Kbytes/s
+            case 13:
+            // Sent Kbytes/s
+                item = item.toFixed(2);
+                break;
+        }
+        return item;
+    }, [[0, 0]], 0, summaryTableHeader);
+
+    // Create error table
+    createTable($("#errorsTable"), {"supportsControllersDiscrimination": false, "titles": ["Type of error", "Number of errors", "% in errors", "% in all samples"], "items": []}, function(index, item){
+        switch(index){
+            case 2:
+            case 3:
+                item = item.toFixed(2) + '%';
+                break;
+        }
+        return item;
+    }, [[1, 1]]);
+
+        // Create top5 errors by sampler
+    createTable($("#top5ErrorsBySamplerTable"), {"supportsControllersDiscrimination": false, "overall": {"data": ["Total", 50, 0, "", "", "", "", "", "", "", "", "", ""], "isController": false}, "titles": ["Sample", "#Samples", "#Errors", "Error", "#Errors", "Error", "#Errors", "Error", "#Errors", "Error", "#Errors", "Error", "#Errors"], "items": [{"data": [], "isController": false}, {"data": [], "isController": false}, {"data": [], "isController": false}, {"data": [], "isController": false}, {"data": [], "isController": false}]}, function(index, item){
+        return item;
+    }, [[0, 0]], 0);
+
+});
